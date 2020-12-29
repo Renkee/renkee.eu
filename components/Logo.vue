35f58a9917bae7cfe2b50af3bcf6a2fa0fe13631
René Lajvort <rene@renkee.eu>
2020-12-29 09:39:24 +0100

    recreate my logo in html/css

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,35 +1,81 @@
 <template>
-  <svg
-    class="NuxtLogo"
-    width="245"
-    height="180"
-    viewBox="0 0 452 342"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M139 330l-1-2c-2-4-2-8-1-13H29L189 31l67 121 22-16-67-121c-1-2-9-14-22-14-6 0-15 2-22 15L5 303c-1 3-8 16-2 27 4 6 10 12 24 12h136c-14 0-21-6-24-12z"
-      fill="#00C58E"
-    />
-    <path
-      d="M447 304L317 70c-2-2-9-15-22-15-6 0-15 3-22 15l-17 28v54l39-67 129 230h-49a23 23 0 0 1-2 14l-1 1c-6 11-21 12-23 12h76c3 0 17-1 24-12 3-5 5-14-2-26z"
-      fill="#108775"
-    />
-    <path
-      d="M376 330v-1l1-2c1-4 2-8 1-12l-4-12-102-178-15-27h-1l-15 27-102 178-4 12a24 24 0 0 0 2 15c4 6 10 12 24 12h190c3 0 18-1 25-12zM256 152l93 163H163l93-163z"
-      fill="#2F495E"
-    />
-  </svg>
+  <div id="logo-container">
+    <div class="colored-rectangle rect-6"></div>
+    <div class="colored-rectangle rect-5"></div>
+    <div class="colored-rectangle rect-4"></div>
+    <div class="colored-rectangle rect-3"></div>
+    <div class="colored-rectangle rect-2"></div>
+    <div class="colored-rectangle rect-1">
+      <div id="text-container">
+        <div id="text">rnk</div>
+        <div id="underline"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-.NuxtLogo {
-  animation: 1s appear;
-  margin: auto;
+<style lang="scss" scoped>
+@font-face {
+  font-family: 'Harabara Mais Demo';
+  src: url('/logo_font.otf') format('opentype');
 }
-
-@keyframes appear {
-  0% {
-    opacity: 0;
+#text-container {
+  position: absolute;
+  inset: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  #text {
+    color: white;
+    font-family: 'Harabara Mais Demo', sans-serif;
+    line-height: 0.625;
+    font-size: 320px;
+    text-shadow: 0px 0px 1px rgba(0, 0, 0, 0.55);
   }
+  #underline {
+    background-color: white;
+    height: 24px;
+    width: 424px;
+    margin-left: 13px;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+    box-shadow: 0px 0px 1px 0px rgba(0, 0, 0, 0.55);
+  }
+}
+#logo-container {
+  position: relative;
+  padding: 150px;
+
+  & > .colored-rectangle {
+    position: absolute;
+    width: 500px;
+    height: 500px;
+    opacity: 0.87;
+  }
+}
+.rect-1 {
+  background-color: #34495e;
+  position: relative;
+}
+.rect-2 {
+  background-color: #c0392b;
+  transform: rotate(15deg);
+}
+.rect-3 {
+  background-color: #9b59b6;
+  transform: rotate(30deg);
+}
+.rect-4 {
+  background-color: #2980b9;
+  transform: rotate(45deg);
+}
+.rect-5 {
+  background-color: #f1c40f;
+  transform: rotate(60deg);
+}
+.rect-6 {
+  background-color: #f39c12;
+  transform: rotate(75deg);
 }
 </style>
